81a333ec7727c5df9d88855a619eb507c3bbb8ba
Gogo <sebastian.segovia@upb.edu.co>
2023-09-06 18:08:35 -0500

    FirebaseDatabase

--- SID_UNITY/Assets/Scripts/Firebase/GameAuthResponse.cs
+++ SID_UNITY/Assets/Scripts/Firebase/GameAuthResponse.cs
@@ -1,12 +1,9 @@
 using Firebase.Auth;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class GameAuthResponse : MonoBehaviour
 {
--- SID_UNITY/Assets/Scripts/Firebase/HomeAuthResponse.cs
+++ SID_UNITY/Assets/Scripts/Firebase/HomeAuthResponse.cs
@@ -1,76 +1,91 @@
 using Firebase.Auth;
+using Firebase.Database;
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class HomeAuthResponse : MonoBehaviour
 {
-    private string sceneToLoad = "SceneGame";
+    private string sceneToLoad = "GameScene";
 
     private Button buttonLogin;
     private Button buttonSingUp;
 
-    private TMP_InputField inputFieldEmail;
-    private TMP_InputField inputFieldPassword;
+    DatabaseReference mDatabaseRef;
 
     void Start()
     {
         buttonLogin = GameObject.Find("Button_Login").GetComponent<Button>();
         buttonSingUp = GameObject.Find("Button_SignUp").GetComponent<Button>();
-        inputFieldEmail = GameObject.Find("InputField_Email").GetComponent<TMP_InputField>();
-        inputFieldPassword = GameObject.Find("InputField_Password").GetComponent<TMP_InputField>();
 
         buttonSingUp.onClick.AddListener(HandleSingUpButtonClicked);
         buttonLogin.onClick.AddListener(HandleLoginButtonClicked);
+
+        mDatabaseRef = FirebaseDatabase.DefaultInstance.RootReference;
     }
 
     private void HandleSingUpButtonClicked()
     {
-        var auth = FirebaseAuth.DefaultInstance;
-        auth.CreateUserWithEmailAndPasswordAsync(inputFieldEmail.text, inputFieldPassword.text).ContinueWith(task => {
-            if (task.IsCanceled)
-            {
-                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
-                return;
-            }
-            if (task.IsFaulted)
-            {
-                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
-                return;
-            }
+        string email = GameObject.Find("InputField_Email").GetComponent<TMP_InputField>().text;
+        string username = GameObject.Find("InputField_Username").GetComponent<TMP_InputField>().text;
+        string password = GameObject.Find("InputField_Password").GetComponent<TMP_InputField>().text;
 
-            // Firebase user has been created.
-            Firebase.Auth.AuthResult result = task.Result;
-            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
-                result.User.DisplayName, result.User.UserId);
-        });
+        StartCoroutine(RegisterUser(email, username, password));
     }
 
     private void HandleLoginButtonClicked()
     {
-        var auth = FirebaseAuth.DefaultInstance;
-        auth.SignInWithEmailAndPasswordAsync(inputFieldEmail.text, inputFieldPassword.text).ContinueWith(task => {
-            if (task.IsCanceled)
-            {
-                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
-                return;
-            }
-            if (task.IsFaulted)
-            {
-                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
-                return;
-            }
+        string email = GameObject.Find("InputField_Email").GetComponent<TMP_InputField>().text;
+        string username = GameObject.Find("InputField_Username").GetComponent<TMP_InputField>().text;
+        string password = GameObject.Find("InputField_Password").GetComponent<TMP_InputField>().text;
 
-            Firebase.Auth.AuthResult result = task.Result;
+        StartCoroutine(LoginUser(email, username, password));
+    }
+
+    private IEnumerator RegisterUser(string email, string username, string password)
+    {
+        var auth = FirebaseAuth.DefaultInstance;
+        var registerTask = auth.CreateUserWithEmailAndPasswordAsync(email, password);
+        yield return new WaitUntil(() => registerTask.IsCompleted);
+        if (registerTask.IsCanceled)
+            Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+        else if (registerTask.IsFaulted)
+            Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + registerTask.Exception);
+        else
+        {
+            Firebase.Auth.AuthResult result = registerTask.Result;
+            string name = GameObject.Find("InputField_Username").GetComponent<TMP_InputField>().text;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                name, result.User.UserId);
+
+            mDatabaseRef.Child("users").Child(result.User.UserId).Child("username").SetValueAsync(username);
+        }
+    }
+
+    private IEnumerator LoginUser(string email, string username, string password)
+    {
+        var auth = FirebaseAuth.DefaultInstance;
+        var registerTask = auth.SignInWithEmailAndPasswordAsync(email, password);
+        yield return new WaitUntil(() => registerTask.IsCompleted);
+        if (registerTask.IsCanceled)
+            Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+        else if (registerTask.IsFaulted)
+            Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + registerTask.Exception);
+        else
+        {
+            Firebase.Auth.AuthResult result = registerTask.Result;
             Debug.LogFormat("User signed in successfully: {0} ({1})",
-                result.User.DisplayName, result.User.UserId);
+                username, result.User.UserId);
 
             SceneManager.LoadScene(sceneToLoad);
-        });
+        }
+
+            
+            
+            
+        
 
         
     }
